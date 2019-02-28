c0504f8591ea85c48d1e9ad0be72a273177a28fa
Anwar Montasir <anwarmontasir@gmail.com>
2019-02-28 11:34:40 -0800

    add flexbox to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.css';
+import Split from './composition/Split';
 
 function App() {
     return (
         <main className='App'>
-          { /* content goes here */ }
+          <Split className='left' flexBasis={2}>
+            This is the content for the left Split. Lorem ipsum.
+          </Split>
+          <Split className='right'>
+            This is the content for the right Split. Lorem ipsum.
+          </Split>
         </main>
     );
 }
